8a956ac01530c1d38782be4ee2f44c231636c4ad
ong-ar <reclusis@gmail.com>
2019-05-08 23:06:13 +0900

    탈중앙화를 위해 P2P 적용

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
   morgan = require("morgan"),
-  Blockchain = require("./blockchain");
+  Blockchain = require("./blockchain"),
+  P2P = require("./p2p");
 
 const { getBlockchain, createNewBlock } = Blockchain;
+const { startP2PServer } = P2P;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -24,4 +26,8 @@ app.post("/blocks", (req, res) => {
   res.send(newBlock);
 });
 
-app.listen(PORT, () => console.log("Nomadcoin server running on", PORT));
+const server = app.listen(PORT, () =>
+  console.log(`Nomadcoin server running on ${PORT}`)
+);
+
+startP2PServer(server);
